8b63ee0a6c23e65543af12636438e5206472d495
Maksim <110735975+racingulitka@users.noreply.github.com>
2022-10-04 20:50:55 +0300

    add burger menu in mobile version

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/*Открытие основного меню*/
+function open1(){
+	let uslovie;
+	uslovie = getComputedStyle(menu_open_check1).display;
+	if (uslovie == "none"){ 
+		document.getElementById('menu').style.cssText = 
+			"transform:rotate(0deg);border-radius:50%; animation:rotateMenu1 0.5s ease; font-weight:400;padding-top:5px;";
+		document.getElementById("menu").innerHTML="X"
+		for(let a=1;a<=6;a++){
+			document.getElementById(`menu_open_check${a}`).style.cssText = 
+				"display:block; animation:fadeIn 1s ease;";
+		}
+	}
+	else{
+		document.getElementById('menu').style.cssText = 
+			"transform:rotate(90deg); animation:rotateMenu2 0.5s ease";
+		document.getElementById("menu").innerHTML="|||"
+		for(let b=1; b<=6; b++){
+			document.getElementById(`menu_open_check${b}`).style.cssText = 
+				"display:none;";
+		}
+	}
+}
