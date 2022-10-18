64129da2e145b40baeaa8c6ff06801aa1a6dae15
Maksim <110735975+racingulitka@users.noreply.github.com>
2022-10-18 22:32:06 +0300

    add ceiling calculator page
    add ceiling calculator page

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function open1(){
 		document.getElementById('menu').style.cssText = 
 			"transform:rotate(0deg);border-radius:50%; animation:rotateMenu1 0.5s ease; font-weight:400;padding-top:5px;";
 		document.getElementById("menu").innerHTML="X"
-		for(let a=1;a<=6;a++){
+		for(let a=1;a<=7;a++){
 			document.getElementById(`menu_open_check${a}`).style.cssText = 
 				"display:block; animation:fadeIn 1s ease;";
 		}
@@ -15,7 +15,7 @@ function open1(){
 		document.getElementById('menu').style.cssText = 
 			"transform:rotate(90deg); animation:rotateMenu2 0.5s ease";
 		document.getElementById("menu").innerHTML="|||"
-		for(let b=1; b<=6; b++){
+		for(let b=1; b<=7; b++){
 			document.getElementById(`menu_open_check${b}`).style.cssText = 
 				"display:none;";
 		}
---
+++ js/script_ceiling_calculator.js
@@ -0,0 +1,127 @@
+//ad variables
+let total, square, maxWidth, corners, pipes, spotLights, bigLight, cornice, ceramicTiles, timber, colorRoof, credit;
+
+window.onload = function(){
+squareCostFunction();
+cornersCostFunction();
+pipesCostFunction();
+spotLightsCostFunction();
+bigLightCostFunction();
+corniceCostFunction();
+ceramicTilesCostFunction();
+timberCostFunction();
+creditCostFunction();
+totalFunction();
+}
+
+//calculate values in rightContainers
+
+function squareCostFunction(){
+	if (document.getElementById("square").value <=2){
+		document.getElementById("squareCost").innerHTML = document.getElementById("square").value * 1500;
+	}
+	else if (document.getElementById("square").value <= 4){
+		document.getElementById("squareCost").innerHTML = document.getElementById("square").value * 800;
+	}
+	else if (document.getElementById("square").value <= 30){
+		if (document.getElementById("maxWidth").checked == true || document.getElementById("colorRoof").checked == true){
+			document.getElementById("squareCost").innerHTML = document.getElementById("square").value * 900;
+		}
+		else {
+			document.getElementById("squareCost").innerHTML = document.getElementById("square").value * 700;
+		}
+	}
+	else {
+		if (document.getElementById("maxWidth").checked == true || document.getElementById("colorRoof").checked == true){
+			document.getElementById("squareCost").innerHTML = document.getElementById("square").value * 850;
+		}
+		else {
+			document.getElementById("squareCost").innerHTML = document.getElementById("square").value * 650;
+		}
+	}
+	square = document.getElementById("squareCost").innerHTML;
+}
+
+function cornersCostFunction(){
+	if (document.getElementById("corners").value > 4){
+		document.getElementById("cornersCost").innerHTML = (document.getElementById("corners").value - 4) * 100;
+	}
+	else{
+		document.getElementById("cornersCost").innerHTML = 0;
+	}
+	corners = document.getElementById("cornersCost").innerHTML;
+}
+
+function pipesCostFunction(){
+	if (document.getElementById("pipes").value > 3){
+		document.getElementById("pipesCost").innerHTML = (document.getElementById("pipes").value - 3) * 100;
+	}
+	else{
+		document.getElementById("pipesCost").innerHTML = 0;
+	}
+	pipes = document.getElementById("pipesCost").innerHTML;
+}
+
+function spotLightsCostFunction(){
+	if ((document.getElementById("square").value / 3) < document.getElementById("spotLights").value){
+		document.getElementById("spotLightsCost").innerHTML = (document.getElementById("spotLights").value - Math.floor((document.getElementById("square").value / 3))) * 400;
+		}
+	else {
+			document.getElementById("spotLightsCost").innerHTML = 0;
+	}
+	spotLights = document.getElementById("spotLightsCost").innerHTML;
+}
+
+function bigLightCostFunction(){
+	if (document.getElementById("bigLight").checked == true){
+		document.getElementById("bigLightCost").innerHTML = 500;
+	}
+	else{
+		document.getElementById("bigLightCost").innerHTML = 0;
+	}
+	bigLight = document.getElementById("bigLightCost").innerHTML;
+}
+
+function corniceCostFunction(){
+	if (document.getElementById("cornice").value > 0){
+		document.getElementById("corniceCost").innerHTML = document.getElementById("cornice").value * 350;
+	}
+	else{
+		document.getElementById("corniceCost").innerHTML = 0;
+	}
+	cornice = document.getElementById("corniceCost").innerHTML;
+}
+
+function ceramicTilesCostFunction(){
+	if (document.getElementById("ceramicTiles").value > 0){
+		document.getElementById("ceramicTilesCost").innerHTML = document.getElementById("ceramicTiles").value * 150;
+	}
+	else{
+		document.getElementById("ceramicTilesCost").innerHTML = 0;
+	}
+	ceramicTiles = document.getElementById("ceramicTilesCost").innerHTML;
+}
+
+function timberCostFunction(){
+	if (document.getElementById("timber").value > 0){
+		document.getElementById("timberCost").innerHTML = document.getElementById("timber").value * 300;
+	}
+	else{
+		document.getElementById("timberCost").innerHTML = 0;
+	}
+	timber = document.getElementById("timberCost").innerHTML;
+}
+
+function creditCostFunction(){
+	if (document.getElementById("credit").checked == true){
+		document.getElementById("creditCost").innerHTML = 1000;
+	}
+	else{
+		document.getElementById("creditCost").innerHTML = 0;
+	}
+	credit = document.getElementById("creditCost").innerHTML;
+}
+
+function totalFunction(){
+	document.getElementById("total").innerHTML = (Number(square) + Number(corners) + Number(pipes) + Number(spotLights) + Number(bigLight) + Number(cornice) + Number(ceramicTiles) + Number(timber) + Number(credit)) + " рублей";
+}
